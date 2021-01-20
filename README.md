c00911666ead448d71d51da33323d69d390292f0
AaryaPednekar <75307469+AaryaPednekar@users.noreply.github.com>
2021-01-20 19:48:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+LAVENDER GIRLS
+
+"We Fly So High, We Fly Together..
+    We Are A Girl Gang Like
+       BIRDS OF A FEATHER"
+
---
+++ sketch.js
@@ -0,0 +1,11 @@
+var lg
+function preload(){
+lg.loadImage("LG.png")
+}
+function setup(){
+    createCanvas(100,100)
+image(lg,50,50,20,20)
+}
+function draw(){
+drawSprites();
+}
